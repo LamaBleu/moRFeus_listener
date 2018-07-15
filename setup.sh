7604c0ea005096270f691589710e8307b2fdfabb
LamaBleu <lama.bleu@gmail.com>
2018-07-15 16:07:06 +0200

    outernet.is --> othernet.is

--- setup.sh
+++ setup.sh
@@ -12,13 +12,13 @@ echo -n "Choose OS  32bits, 64bits or armv7 (Raspberry) [32/64/arm]:  "
 read platform
 case "$platform" in
 	32) echo "32 bits"
-	    wget -O $morf_tool_path/morfeus_tool https://archive.outernet.is/morfeus_tool_v1.6/morfeus_tool_linux_x32;
+	    wget -O $morf_tool_path/morfeus_tool https://archive.othernet.is/morfeus_tool_v1.6/morfeus_tool_linux_x32;
 	    ;;
 	64) echo "64 bits"
-	    wget -O $morf_tool_path/morfeus_tool https://archive.outernet.is/morfeus_tool_v1.6/morfeus_tool_linux_x64;
+	    wget -O $morf_tool_path/morfeus_tool https://archive.othernet.is/morfeus_tool_v1.6/morfeus_tool_linux_x64;
 	    ;;
 	arm) echo "arm"
-	    wget -O $morf_tool_path/morfeus_tool https://archive.outernet.is/morfeus_tool_v1.6/morfeus_tool_linux_armv7;
+	    wget -O $morf_tool_path/morfeus_tool https://archive.othernet.is/morfeus_tool_v1.6/morfeus_tool_linux_armv7;
             ;;
 	  *) echo "SORRY ! 32  64 or arm only  ! (or manual download : https:/archive.outernet.is/morfeus_tool_v1.6/ )"
 	     exit 0;
